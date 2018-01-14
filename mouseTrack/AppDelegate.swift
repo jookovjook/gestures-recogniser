6e8bfbd838aea032170ebe388a537321d1e27260
Жека <jookovjook@gmail.com>
2018-01-14 10:42:25 +0300

    Initial Commit

---
+++ mouseTrack/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mouseTrack
+//
+//  Created by Жека on 14/01/2018.
+//  Copyright © 2018 Жека. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mouseTrack/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mouseTrack
+//
+//  Created by Жека on 14/01/2018.
+//  Copyright © 2018 Жека. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
